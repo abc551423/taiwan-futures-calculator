750ee303b4970cd91a9e7bcc6c66b076ef34e581
unknown <abcd972831@gmail.com>
2026-07-22 05:07:21 +0800

    chore: add adsense verification script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -71,11 +70,10 @@ export default function RootLayout({
       lang="zh-TW"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+
       <head>
-        <Script
-          id="adsense"
+        <script
           async
-          strategy="beforeInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5460769982666546"
           crossOrigin="anonymous"
         />
